ae5a1380ebde99e204e62e92f6ff92aba3657526
Alejandro Sior <aho@sior.be>
2020-05-20 21:30:33 +0200

    Optimized and shortened code

--- mod.ts
+++ mod.ts
@@ -1,44 +1,24 @@
-function init(m: number, n: number): number[][] {
-  let d: number[][] = [];
-  for (let i = 0; i <= m; i++) {
-    let arr: number[] = [];
-    for (let j = 0; j <= n; j++) {
-      arr.push(0);
-    }
-    d.push(arr);
-  }
-  return d;
-}
-
-function minimum(numbers: number[]): number {
-  let min = numbers[0];
-  for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] < min) min = numbers[i];
-  }
-  return min;
-}
-
 export default function levenshtein(s: string, t: string): number {
   if (s === t) return 0;
-  let d: number[][] = init(s.length, t.length);
+  const sl = s.length + 1;
+  const tl = t.length + 1;
+  let d: number[] = new Array(sl * tl).fill(0);
 
   for (let i = 1; i <= s.length; i++) {
-    d[i][0] = i;
+    d[tl * i] = i;
   }
 
   for (let i = 1; i <= t.length; i++) {
-    d[0][i] = i;
+    d[i] = i;
   }
 
   for (let j = 1; j <= t.length; j++) {
     for (let i = 1; i <= s.length; i++) {
       let substitutionCost = 1;
       if (s[i - 1] === t[j - 1]) substitutionCost = 0;
-      d[i][j] = minimum(
-        [d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + substitutionCost],
-      );
+      d[tl * i + j] = Math.min(d[tl * (i - 1) + j] + 1, d[tl * i + j - 1] + 1, d[tl * (i - 1) + j - 1] + substitutionCost);
     }
   }
 
-  return d[s.length][t.length];
+  return d[sl * tl - 1];
 }
--- mod_tests.ts
+++ mod_tests.ts
@@ -2,11 +2,18 @@ import {
   assertStrictEq,
 } from "https://deno.land/std/testing/asserts.ts";
 
-import levenshstein from "./mod.ts";
+import levenshtein from "./mod.ts";
 
 Deno.test("testing sanity with cases", () => {
-  assertStrictEq(levenshstein("", ""), 0);
-  assertStrictEq(levenshstein("hello", "hallo"), 1);
-  assertStrictEq(levenshstein("ana", "yyy"), 3);
-  assertStrictEq(levenshstein("ana", "ayy"), 2);
+  assertStrictEq(levenshtein("", ""), 0);
+  assertStrictEq(levenshtein("ab", "ab"), 0);
+  assertStrictEq(levenshtein("tabad", "tabd"), 1);
+  assertStrictEq(levenshtein("hello", "hallo"), 1);
+  assertStrictEq(levenshtein("ana", "yyy"), 3);
+  assertStrictEq(levenshtein("ana", "ayy"), 2);
+  assertStrictEq(levenshtein("akak", "kak"), 1);
+  assertStrictEq(levenshtein("quick", "quiker"), 3);
+  assertStrictEq(levenshtein("arch", "arkh"), 1);
+  assertStrictEq(levenshtein("arch", "abkh"), 2);
+  assertStrictEq(levenshtein("kitten", "sitting"), 3);
 });
